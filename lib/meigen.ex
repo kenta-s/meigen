c16b2a67d3eff0e3781f3fc15a3344e30bf2e4e6
kenta-s <knt01222@gmail.com>
2017-03-08 21:58:35 +0900

    Initial commit

---
+++ lib/meigen.ex
@@ -0,0 +1,18 @@
+defmodule Meigen do
+  @moduledoc """
+  Documentation for Meigen.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Meigen.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/meigen_test.exs
@@ -0,0 +1,8 @@
+defmodule MeigenTest do
+  use ExUnit.Case
+  doctest Meigen
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
