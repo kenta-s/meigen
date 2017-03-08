1761595a565f53f0a83352fdcf9d94d019a3c36e
kenta-s <knt01222@gmail.com>
2017-03-08 23:04:52 +0900

    Remove some comments

--- lib/meigen.ex
+++ lib/meigen.ex
@@ -1,21 +1,4 @@
 defmodule Meigen do
-  @moduledoc """
-  Documentation for Meigen.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Meigen.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   def pick(file) do
     [meigen] = File.stream!(file) |> Enum.to_list |> Enum.shuffle |> Enum.take(1)
     meigen
